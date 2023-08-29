718545080525c9c6d0f9659d22ae6a37e9dd8e41
Roberto Olmos <olmosrobertoalejandro@gmail.com>
2023-08-29 10:57:38 -0300

    parte 3

--- practico2/README.md
+++ practico2/README.md
@@ -10,4 +10,7 @@ Aca va estar el informe o documentación de TPN2
 ## PARTE 2: Obtención del nombre del jugador
 * `main.js` archivos para las acciones dinámicas de la app:
     * `agregarNombre()`: función para tomar el nombre del usuario. Contiene un test. Está vinculada a un evento de tipo "onclick" en el *input* con `id="nombre"`.
+
+## PARTE 3: Selección de la opción del jugador y del oponente (PC)
+* Crea 3 imagenes "Piedra", "Papel" y "Tijeras" que al evento click ejecutan la funcion juegaPiedra(), juegaPapel() o juegaTijeras() que determinan la jugada del usuario y ejecuta la funcion obtenerJugadaComputadora() eligiendo un número random del 0 al 2, siendo 0 = piedra, 1 = papel y 2 = tijeras
 [Esta es mi página]: 
--- practico2/main.js
+++ practico2/main.js
@@ -8,6 +8,54 @@ function agregarNombre(){
      * Condición de testeo para la contención del nombre
      */
     if(nombre !== null) {
-        console.log(nombre;)
+        console.log(nombre)
     }
 }
+let piedra = document.getElementById(piedra);
+let papel = document.getElementById(papel);
+let tijeras = document.getElementById(tijeras);;
+
+
+function juegaPiedra() {
+    document.getElementById('nombre').innerHTML = `${nombre} eligio:`;
+    let jugadaUsuario = usuario.innerHTML = 'piedra';
+    // console.log(jugadaUsuario);
+    let jugadaComputadora = obtenerJugadaComputadora();
+    // console.log(jugadaComputadora);
+}
+function juegaPapel() {
+    document.getElementById('nombre').innerHTML = `${nombre} eligio:`;
+    let jugadaUsuario = usuario.innerHTML = 'papel';
+    // console.log(jugadaUsuario);
+    let jugadaComputadora = obtenerJugadaComputadora();
+    // console.log(jugadaComputadora);
+}
+function juegaTijeras() {
+    document.getElementById('nombre').innerHTML = `${nombre} eligio:`;
+    let jugadaUsuario = usuario.innerHTML = 'tijeras';
+    // console.log(jugadaUsuario);
+    let jugadaComputadora = obtenerJugadaComputadora();
+    // console.log(jugadaComputadora);
+}
+// Funcion que determina un numero ramdon entre 1 y 2.
+function getRandomInt() {
+    return Math.floor(Math.random() * 2);
+}
+
+// Funcion que hace que la computadora elija aleatoriamente entre piedra, papel o tijera.
+function obtenerJugadaComputadora() {
+    let jugadaComputadora = getRandomInt();
+    if (jugadaComputadora === 0) {
+        jugadaComputadora = computadora.innerHTML = 'piedra';
+        // console.log(jugadaComputadora)
+        return 'piedra'
+    } else if (jugadaComputadora === 1) {
+        jugadaComputadora = computadora.innerHTML = 'papel';
+        // console.log(jugadaComputadora)
+        return 'papel'
+    } else {
+        jugadaComputadora = computadora.innerHTML = 'tijeras';
+        // console.log(jugadaComputadora)
+        return 'tijera'
+    }
+};
